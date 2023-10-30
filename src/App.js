667c94ebd894cfa9b9d2878f55f92e2de1f089a5
elias <eliasoliveiradapaz@hotmail.com>
2023-10-30 16:25:25 -0300

    archives commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Header</header>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
